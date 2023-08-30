0247b39fffd4c8e9344e9745a3310a5ed5398d5f
michalMudr <270074346@yoobeestudent.ac.nz>
2023-08-30 19:15:34 +1200

    User Registration Final

--- base/views.py
+++ base/views.py
@@ -19,7 +19,7 @@ from django.contrib.auth.forms import UserCreationForm
 def loginPage(request):
     page = 'login'
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         
         try:
@@ -44,6 +44,18 @@ def logoutUser(request):
 
 def registerPage(request):
     form = UserCreationForm()
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('UserDashboard')
+        else:
+            messages.error(request, ' An error occured during registration')
+            
     return render(request, 'base/login_register.html', {'form': form})
 
 def home(request):
